47de6ce936177db386bdcdc7ed3a1f19aef926b5
gugmin <149454489+gugmin@users.noreply.github.com>
2023-11-29 10:17:04 +0900

    fixbug
    layer 설정

--- Assets/Scripts/Interaction.cs
+++ Assets/Scripts/Interaction.cs
@@ -9,13 +9,21 @@ public class Interaction : MonoBehaviour
     {
         if (other.gameObject.tag == "Player") //플레이어와 충돌 감지
         {
-            TalkPanel.SetActive(true); //해당 카메라 ON
+            TalkPanel.SetActive(true); 
+        }
+        else if (other.gameObject.tag == "Player1") //플레이어와 충돌 감지
+        {
+            TalkPanel.SetActive(true);
         }
     }
 
     private void OnTriggerExit2D(Collider2D other) //영역 밖으로 나갈때 발생하는 코드
     {
         if (other.gameObject.tag == "Player") //플레이어와 충돌 감지
+        {
+            TalkPanel.SetActive(false);
+        }
+        else if (other.gameObject.tag == "Player1") //플레이어와 충돌 감지
         {
             TalkPanel.SetActive(false);
         }
